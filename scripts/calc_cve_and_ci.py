2e4d524fda5f349071f8e023054ffc0a4bb804d5
poorya mirzavand <pooryamb@gmail.com>
2026-01-21 21:33:26 -0500

    improved parallel computation of cve calculation script

--- scripts/calc_cve_and_ci.py
+++ scripts/calc_cve_and_ci.py
@@ -5,29 +5,20 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import resample
 from scipy.interpolate import interp1d
-from joblib import Parallel, delayed
+from multiprocessing import Pool, cpu_count
 from cve import cve
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(script_dir, "../data")
 
-# def naac(x, y, y_max):
-    # """Calculates the normalized area above the curve. It is calculated by first calculating the AUC and then subtracting it from 
-    # the area of the box surrounding the plot"""
-    # auc = np.trapz(np.log10(y), x) # Calculate the AUC
-    # x_max = x.iloc[-1] if isinstance(x, pd.Series) else x[-1]
-    # x_min = x.iloc[0 ] if isinstance(x, pd.Series) else x[0 ]
-    # aac = y_max * (x_max-x_min) - auc  # Calculate the Area Above the Curve
-    # return aac / y_max                 # Returns the normalized area above the curve
- 
-  
-def bootstrap_once(i, q_grouped_df, cov_x, q_count): #, y_max = 10): y_max was supposed to be used for AAC, which was opted to set aside for now
+
+def bootstrap_once(i, q_grouped_df, cov_x, q_count):
     """Selects a random sample of queries and interpolates the fpepq based on the cov_x values fed into it
     q_count is needed because the number of queries in bootstraped samples will not be equal to the unique number of queries.
     As for sampling the dataframe, we need to group the dataframe, we group it only once so that in each sampling, we just
     combine the groups that we need.
     """
-    
+
     selected_qs = resample(pd.Series(q_grouped_df.keys()), replace=True, random_state=i) # Select a sample of the queries
     query_counts = selected_qs.value_counts()   # Find the number of times each query has been sampled
 
@@ -45,59 +36,32 @@ def bootstrap_once(i, q_grouped_df, cov_x, q_count): #, y_max = 10): y_max was s
     )
     
     boot_interp_fpepq = interp_cve(cov_x)
-    
-    # interp_cve_inv = interp1d(
-        # cve_boot_df["fpepq_pfam"], cve_boot_df["cov_pfam"], 
-        # bounds_error=False, fill_value="extrapolate", assume_sorted=False
-    # )
-    
-    # max_x = interp_cve_inv(10)  # First find the cov value which corresponds to FPEPQ=10
-    # cve_boot_df4auc = cve_boot_df[cve_boot_df["fpepq_pfam"]<10][["fpepq_pfam", "cov_pfam"]]
-    # cve_boot_df4auc.loc[len(cve_boot_df4auc)] = [10, max_x] # Add a row for the FPEPQ=10
-    
-    # naac_bootstrap = naac(cve_boot_df["cov_pfam"], cve_boot_df["fpepq_pfam"], 10)
-    
-    return boot_interp_fpepq #, naac_bootstrap
-    
+
+    return boot_interp_fpepq
+
 
 
 def bootstrap_cve_parallel(df, cov_x, n_bootstrap=1000, n_jobs=None):
-    "This runs the function in parallel"""
+    """This runs the function in parallel"""
     q_count = df["query"].nunique()
     q_grouped_df = {grp_name: grp_df for grp_name, grp_df in df.groupby("query")}
-        
-    results = Parallel(n_jobs=-1)(
-        delayed(bootstrap_once)(i, q_grouped_df, cov_x, q_count) for i in range(n_bootstrap)
-    )
-    
+
+    with Pool(processes=cpu_count()) as pool:
+        results = pool.starmap(
+            bootstrap_once,
+            [(i, q_grouped_df, cov_x, q_count) for i in range(n_bootstrap)]
+        )
+
     fpepq_results = pd.DataFrame([r for r in results if r is not None])
-    #naac_results = [r[1] for r in results if r is not None]
-    return fpepq_results #, naac_results
-    
-    #results = []    
-    #for i in range(n_bootstrap):
-    #    results.append(bootstrap_once(i, q_grouped_df, cov_x, q_count))
 
-    """
-    with ProcessPoolExecutor(max_workers=n_jobs) as executor:
-        futures = [executor.submit(bootstrap_once, i, q_grouped_df, cov_x, q_count) for i in range(n_bootstrap)]
-
-        for fut in futures:
-            try:
-                r = fut.result()
-                if r is not None:
-                    results.append(r)
-            except Exception as e:
-                print(f"[Future error] {e}")
-    """
-    #return pd.DataFrame(results)
+    return fpepq_results
 
 
 # First, read all data from e-value bins and concatenate them to make a single dataframe
 evalue_bins_path = f"{data_dir}/processed/evalue_bins/"
 file_paths = glob.glob(f"{evalue_bins_path}/*.tsv")
 tools = sorted(list(set(os.path.basename(x).split("_B")[0] for x in file_paths)))
-tools_paths = {x: sorted(glob.glob(f"{evalue_bins_path}/{x}*.tsv")) for x in tools}
+tools_paths = {x: sorted(glob.glob(f"{evalue_bins_path}/{x}_B*.tsv")) for x in tools}
 # Files of each batch are read and concatenated together.
 tools_df = {}
 for tool in tools:
@@ -105,7 +69,7 @@ for tool in tools:
     tools_df[tool] = pd.concat(dfs)
 
 
-upper_fpepq_threshold = 10
+upper_fpepq_threshold = 10 # We want Sensitivity at FPEPQ up to 10
 cve_dict = {}
 # Find the upper and lower quantiles
 ci_value = 0.95
@@ -123,19 +87,13 @@ for tool, df in tools_df.items():
     fpepq_values = fpepq_values[fpepq_values<=upper_fpepq_threshold]
     
     cov_x = interpolated_rel_inv(fpepq_values) # To find the cov_x whose FPEPQ is equal to 10
-    
-    
-    #naac_whole_data = naac(cov_x, fpepq_values, 10)
-    #boot_strapped_covs, boot_strapped_naac  = bootstrap_cve_parallel(df, cov_x) #was used for AAC
+
     boot_strapped_covs  = bootstrap_cve_parallel(df, cov_x) 
 
     lower_ci_cov = boot_strapped_covs.quantile(lower_range)
     upper_ci_cov = boot_strapped_covs.quantile(upper_range)
-    
-    # lower_ci_naac = np.quantile(np.array(boot_strapped_naac), lower_range)
-    # upper_ci_naac = np.quantile(np.array(boot_strapped_naac), upper_range)
-    
-    cve_dict[tool] = [cov_x, fpepq_values, lower_ci_cov, upper_ci_cov] #, naac_whole_data, lower_ci_naac, upper_ci_naac]
+
+    cve_dict[tool] = [cov_x, fpepq_values, lower_ci_cov, upper_ci_cov]
 
 with open(f'{data_dir}/processed/cve_ci.pkl', 'wb') as f:
     pickle.dump(cve_dict, f)
